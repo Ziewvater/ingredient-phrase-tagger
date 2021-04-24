bf247e80d2b0703ca6057a378589be2997ca8a68
Emil Elgaard <shivan@shivan.dk>
2021-04-24 01:58:10 -0400

    add progress to files and clean up imports

--- bin/parse-ingredients.py
+++ bin/parse-ingredients.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 
-import argparse
 import json
 import os
-import sys
 import subprocess
-import tempfile
-import time
+from tqdm import tqdm
 from ingredient_phrase_tagger.training import utils
 
 def safeStr(obj):
@@ -16,9 +13,9 @@ def safeStr(obj):
 def _exec_crf_test(input_text, model_path='/app/models/model.crfmodel'):
 
     try:
-        with open('thefile', mode='w') as input_file:
+        with open('thefile', mode='w',encoding='utf-8') as input_file:
 
-            input_text = [safeStr(line) for line in input_text]
+            # input_text = [safeStr(line) for line in input_text]
 
             input_file.write(utils.export_data(input_text))
             input_file.flush()
@@ -36,25 +33,27 @@ def _convert_crf_output_to_json(crf_output):
 
 
 def main():
-    # raw_ingredient_lines = ["1 (8 ounce) package cream cheese, softened", "3 cups salsa, divided", "4 green onions, chopped, divided", "2\u2009\u00bd cups Cheddar cheese, divided", "2\u2009\u00bd cups shredded Monterey Jack cheese, divided", "12 (8 inch) flour tortillas", "1 cup peanut butter", "1 cup white sugar", "1 egg", "1 cup butter flavored shortening", "\u00be cup white sugar", "\u00be cup brown sugar", "2 eggs", "2 teaspoons Mexican vanilla extract", "2\u2009\u00bc cups all-purpose flour", "1 teaspoon baking soda", "1 teaspoon salt", "2 cups milk chocolate chips", "3 tablespoons apricot preserves", "1 teaspoon fresh ginger paste (such as Gourmet Garden\u2122)", "\u00bd teaspoon minced fresh rosemary", "2 (8 ounce) boneless, skinless chicken breasts", "1 teaspoon vegetable oil", "salt and ground black pepper to taste", "\u00bd cup mayonnaise", "2 tablespoons Sriracha sauce", "1 pound bay scallops (about 36 small scallops)", "1 pinch coarse salt", "1 pinch freshly cracked black pepper", "12 slices bacon, cut into thirds", "1  serving olive oil cooking spray", "2 large russet potatoes, scrubbed", "1 tablespoon peanut oil", "\u00bd teaspoon coarse sea salt", "cooking spray", "\u00bd cup all-purpose flour", "\u00bc cup white sugar", "\u215b cup water", "1 large egg, separated", "1\u2009\u00bd teaspoons melted butter", "\u00bd teaspoon baking powder", "\u00bd teaspoon vanilla extract", "1 pinch salt", "2 tablespoons confectioners' sugar, or to taste", "1 red grapefruit, refrigerated", "1 tablespoon softened butter", "1 tablespoon brown sugar", "2 teaspoons brown sugar", "aluminum foil", "\u00bd teaspoon ground cinnamon", "2 tablespoons coarsely chopped pecans", "1 tablespoon brown sugar", "1 teaspoon all-purpose flour", "\u00bc teaspoon apple pie spice", "2 medium apples, cored and cut into wedges", "1 tablespoon butter, melted"]
+    """Read all the files in inputs folder, place a parsed file in with the same name in the output folder"""
 
     from folder_paths import input_folder,output_folder
 
-    for file in os.listdir('/app/input_texts'):
+    files = os.listdir(input_folder)
 
-        print("Processing "+file)
+    with tqdm(total=len(files)) as bar:
+        for file in files:
 
-        with open(os.path.join(input_folder,file)) as f:
-            raw_ingredient_lines = json.load(f)
+            with open(os.path.join(input_folder,file),encoding='utf-8') as f:
+                raw_ingredient_lines = json.load(f)
 
-        crf_output = _exec_crf_test(raw_ingredient_lines)
-        crf_output = utils.import_data(crf_output.split('\n'))
+            crf_output = _exec_crf_test(raw_ingredient_lines)
+            crf_output = utils.import_data(crf_output.split('\n'))
 
-        file_name = os.path.join(output_folder,file)
+            file_name = os.path.join(output_folder,file)
 
-        with open(file_name, 'w') as f:
-            json.dump(crf_output, f, ensure_ascii=False)
+            with open(file_name, 'w',encoding='utf-8') as f:
+                json.dump(crf_output, f, ensure_ascii=False)
 
+            bar.update(1)
 if __name__ == '__main__':
 
     main()
