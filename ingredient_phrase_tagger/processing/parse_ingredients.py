566f5964c5e4ba91e37358404686e2fb98b400fc
Jeremy <Jeremy.Y.Lawrence@gmail.com>
2023-09-30 12:48:36 -0400

    Make absolute reference to packaged model
    This will allow us to run processing when the package is called from elsewhere. Before this change, the reference to the model wouldn’t work properly if we didn’t call it from the root of this project folder

--- ingredient_phrase_tagger/processing/parse_ingredients.py
+++ ingredient_phrase_tagger/processing/parse_ingredients.py
@@ -6,6 +6,9 @@ import subprocess
 from ingredient_phrase_tagger.training import utils
 from .folder_paths import input_folder, output_folder
 
+PACKAGE_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
+DEFAULT_MODEL_PATH = os.path.join(PACKAGE_DIRECTORY, "model", "model.crfmodel")
+
 
 def _exec_crf_test(
     input_text: str,
@@ -34,10 +37,11 @@ def _convert_crf_output_to_json(crf_output: list[str]):
 def process_lines(
     ingredient_lines: list[str],
     model_path: str = None,
-):
+) -> list[dict[str, str]]:
+    path = model_path or DEFAULT_MODEL_PATH
     crf_output = _exec_crf_test(
         input_text=ingredient_lines,
-        model_path=model_path,
+        model_path=path,
     )
     return _convert_crf_output_to_json(crf_output.split("\n"))
 
@@ -46,12 +50,11 @@ def crf_output_from_file(
     file: str,
     model_path: str = None,
 ):
-    path = model_path or "bin/model/model.crfmodel"
     with open(os.path.join(input_folder, file), encoding="utf-8") as f:
         raw_ingredient_lines = json.load(f)
     return process_lines(
         ingredient_lines=raw_ingredient_lines,
-        model_path=path,
+        model_path=model_path,
     )
 
 
