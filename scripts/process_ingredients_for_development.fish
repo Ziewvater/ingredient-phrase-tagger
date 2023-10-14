dc9e09aa98afba94fb2f021ca1741cec75460a58
Jeremy <Jeremy.Y.Lawrence@gmail.com>
2023-10-14 13:43:09 -0400

    Add helper script

---
+++ scripts/process_ingredients_for_development.fish
@@ -0,0 +1,34 @@
+#!/usr/bin/env fish
+
+# A helper script to process ingredient lines and ingredient names
+
+rm -rf app/output
+mkdir -p app/output
+poetry run python -m ingredient_phrase_tagger.run
+
+# Create input files for ingredient names for double-processing
+for file in app/output/*
+    set -l names (cat $file | jq ".[] | .name")
+    set -l joined_names (string join "," $names)
+
+    # Write the processed names in input
+    set -l filename (path basename $file | path change-extension "")
+    set -l new_filename (printf "app/input/%s_names.json" $filename)
+    printf "[%s]" $joined_names > $new_filename
+
+    # Record new name files to be deleted later
+    set -a cleanup_files $new_filename
+end
+
+# Process ingredient names
+poetry run python -m ingredient_phrase_tagger.run
+
+# Format output files
+for file in app/output/*
+    poetry run python -m json.tool $file $file
+end
+
+# Clean up created input files to avoid polluting future runs
+for filename in $cleanup_files
+    rm $filename
+end
